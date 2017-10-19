2af6993cc6cf0ad94782149abcba12717863e06d
Israelb <yb8200@gmail.com>
2017-10-19 08:11:31 +0300

    Add functionality to the app;

--- js/app.js
+++ js/app.js
@@ -4,9 +4,25 @@
  angular.module('LunchCheck', [])
  .controller('LunchCheckController', LunchCheckControllerFunction);
 
-LunchCheckControllerFunction.$inject = ['$scope']
+LunchCheckControllerFunction.$inject = ['$scope'];
 
  function LunchCheckControllerFunction($scope) {
+   $scope.eatInLunch = "";
+   $scope.message = "";
+   $scope.checkLunch =function () {
+     if ($scope.eatInLunch === "") {
+       $scope.message = "Please enter data first";
+       return;
+     }
 
+      var countOfThingEatInLunch = $scope.eatInLunch.split(',');
+
+      if (countOfThingEatInLunch.length > 0 && countOfThingEatInLunch.length <= 3) {
+        $scope.message = "Enjoy!";
+      } else {
+        $scope.message = "Too much!";
+      }
+
+   }
  }
 })();
